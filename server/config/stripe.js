3d6b070239b8f5453512cf3ce924ddfd7b5116ad
Dhanunjay <dhanunjay@hiresoftsolutions.com>
2025-09-11 20:21:31 +0530

    another commit

--- server/config/stripe.js
+++ server/config/stripe.js
@@ -2,7 +2,7 @@
 import Stripe from 'stripe';
 
 export function getStripe() {
-  const key = process.env.STRIPE_SECRET_KEY;
-  if (!key) throw new Error('STRIPE_SECRET_KEY missing');
+  const key = process.env.STRIPE_URL;
+  if (!key) throw new Error('STRIPE_URL missing');
   return new Stripe(key, { apiVersion: '2024-11-20' });
 }
