21ece662ff8bf5b764b754977a545f1cf0ed21d7
Dhanunjay <dhanunjay@hiresoftsolutions.com>
2025-09-13 14:40:11 +0530

    for deploying purpose

--- server/bootstrap/newsletterStart.js
+++
@@ -1,8 +0,0 @@
-// src/bootstrap/newsletterStart.js
-import NewsletterCampaign from "../models/NewsletterCampaign.js";
-import { scheduleCampaign } from "../controllers/newsletter.controller.js";
-
-export async function bootNewsletterScheduler() {
-  const scheduled = await NewsletterCampaign.find({ status: "scheduled" }).lean();
-  await Promise.all(scheduled.map(scheduleCampaign));
-}
