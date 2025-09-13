cf3e7e78ec06bae11842cce468c4bade13254106
Dhanunjay <dhanunjay@hiresoftsolutions.com>
2025-09-13 14:32:38 +0530

    test

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,5 @@
 // src/routes/index.js
 import { Router } from 'express';
-import classesRoutes from './classes.routes.js'; // ✅ Added .js
 import galleryRouter from './gallery.routes.js';  // ✅ Added .js
 import productsRouter from './products.routes.js'; // ✅ Added .js
 import authRouter from './auth.routes.js';        // ✅ Added .js
@@ -10,7 +9,6 @@ import newsletterRouter from './newsletter.routes.js'; // ✅ Added .js
 const routes = Router();
 
 routes.use('/auth', authRouter);
-routes.use('/classes', classesRoutes);
 routes.use('/gallery', galleryRouter);
 routes.use('/products', productsRouter);
 routes.use('/coupons', couponsRouter);
